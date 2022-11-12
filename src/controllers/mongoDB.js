a598f1d78b22b09e96a066fdef8502aa34cbbbb6
Maxtro2002 <jspr200231@gmail.com>
2022-11-12 16:28:40 -0500

    webtokens agregados

--- src/controllers/mongoDB.js
+++ src/controllers/mongoDB.js
@@ -1,55 +1,61 @@
-const { MongoClient, ObjectId } = require('mongodb')
+const { MongoClient, ObjectId } = require("mongodb");
 
-const uri = "mongodb+srv://hospital:kqCnDNqqo5B2snGo@crappdb.olfywkb.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://hospital:kqCnDNqqo5B2snGo@crappdb.olfywkb.mongodb.net/?retryWrites=true&w=majority";
 
-const mongoClient = new MongoClient(uri)
+const mongoClient = new MongoClient(uri);
 
 const getDocuments = async (dbName, collectionName) => {
-    const db = mongoClient.db(dbName)
-    const collection = db.collection(collectionName)
-    const result = await collection.find({}).toArray();
-    return result
-}
+  const db = mongoClient.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.find({}).toArray();
+  return result;
+};
 
 const getDocumentsWithFilter = async (dbName, collectionName, filter) => {
-    const db = mongoClient.db(dbName)
-    const collection = db.collection(collectionName)
-    const result = await collection.find(filter).toArray();
-    return result
-}
-
+  const db = mongoClient.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.find(filter).toArray();
+  return result;
+};
 
 const getDocumentById = async (dbName, collectionName, id) => {
-    const idMongo = new ObjectId(id)
-    const db = mongoClient.db(dbName)
-    const collection = db.collection(collectionName)
-    const result = await collection.findOne({ _id: idMongo });
-    return result
-}
+  const idMongo = new ObjectId(id);
+  const db = mongoClient.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.findOne({ _id: idMongo });
+  return result;
+};
 
 const insertDocument = async (dbName, collectionName, data) => {
-    const db = mongoClient.db(dbName)
-    const collection = db.collection(collectionName)
-    const result = await collection.insertOne(data);
-    return result
-}
+  const db = mongoClient.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.insertOne(data);
+  return result;
+};
 
 const updateDocumentById = async (dbName, collectionName, { id, data }) => {
-    const idMongo = new ObjectId(id)
-    const db = mongoClient.db(dbName)
-    const collection = db.collection(collectionName)
-    delete data._id
-    const result = await collection.replaceOne({ _id: idMongo }, data);
-    return result
-}
+  const idMongo = new ObjectId(id);
+  const db = mongoClient.db(dbName);
+  const collection = db.collection(collectionName);
+  delete data._id;
+  const result = await collection.replaceOne({ _id: idMongo }, data);
+  return result;
+};
 
 const deleteDocumentById = async (dbName, collectionName, id) => {
-    const idMongo = new ObjectId(id)
-    const db = mongoClient.db(dbName)
-    const collection = db.collection(collectionName)
-    const result = await collection.deleteOne({ _id: idMongo });
-    return result
-}
+  const idMongo = new ObjectId(id);
+  const db = mongoClient.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.deleteOne({ _id: idMongo });
+  return result;
+};
 
-
-module.exports = { getDocuments, insertDocument, getDocumentById, deleteDocumentById, updateDocumentById, getDocumentsWithFilter }
+module.exports = {
+  getDocuments,
+  insertDocument,
+  getDocumentById,
+  deleteDocumentById,
+  updateDocumentById,
+  getDocumentsWithFilter,
+};
